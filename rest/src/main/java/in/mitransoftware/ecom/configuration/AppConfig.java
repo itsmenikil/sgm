c052fb1f555672d1a76f578634eea5c5280c6f41
Sanghvi-Nikil <Sanghvi-Nikil@Nikil>
2017-02-02 04:22:57 -0800

    Spring Data Config

---
+++ rest/src/main/java/in/mitransoftware/ecom/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package in.mitransoftware.ecom.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "in.mitransoftware.ecom")
+public class AppConfig {
+
+}
